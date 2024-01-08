f6df5976ff323ad3af6791e463feb9ddd32ad8cb
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-08 17:15:25 +0100

    feat: lunghezza textarea

--- result.php
+++ result.php
@@ -2,5 +2,27 @@
 $text_area = $_GET['testo'];
 $bad_word = $_GET['nome'];
 
-echo $text_area;
-echo $bad_word;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badword Result</title>
+</head>
+
+<body>
+    <div>
+        <?php echo $text_area; ?>
+        <?php echo strlen($text_area) ?>
+    </div>
+
+    <div>
+        <?php echo $bad_word; ?>
+    </div>
+
+</body>
+
+</html>
