40c6a1d1d426637f6d2d6e746aa9f35039bea787
CaldatoLuca <lucacaldato15@gmail.com>
2024-01-08 18:51:20 +0100

    feat: style

--- index.php
+++ index.php
@@ -4,23 +4,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Badwords</title>
 </head>
 
-<body>
-    <h1>BadWords</h1>
+<body class="d-flex justify-content-center align-items-center flex-column vh-100 bg-warning">
+    <!-- blocco centrale -->
+    <div class="text-center bg-dark p-5 rounded-3 shadow-lg mb-5">
+        <!-- titolo -->
+        <h1 class="text-info mb-3">BadWords</h1>
 
-    <form action="result.php" method="GET">
-        <label for="text">Inserisci il testo</label>
-        <textarea name="testo" id="text" cols="30" rows="10"></textarea>
+        <!-- form -->
+        <form action="result.php" method="GET" class="d-flex justify-content-center align-items-center flex-column">
+            <!-- textarea -->
+            <label for="text" class="form-label text-warning fw-bold">Insert Text Here</label>
+            <div class="input-group mb-3" id="text">
+                <textarea class="form-control" aria-label="With textarea" name="testo"></textarea>
+            </div>
 
-        <label for="badword">Inserisci la parola da eliminare</label>
-        <input type="text" id="badword" name="nome">
+            <!-- input -->
+            <label for="badword" class="form-label text-warning fw-bold">Insert Word to Erase</label>
+            <input type="text" class="form-control mb-4" id="badword" aria-describedby="basic-addon3 basic-addon4" name="nome">
 
-        <button>Send</button>
-    </form>
+            <!-- bottone -->
+            <button class="btn btn-primary">Send</button>
+        </form>
+    </div>
 
-
+    <!-- istruzioni -->
+    <h4 class="fw-bold text-danger">Badwords removes the desired word from a text, replacing it with three asterisks</h4>
+    <h5 class="fw-bold text-danger">It also shows the length of the text before and after editing</h5>
 
 </body>
 
--- result.php
+++ result.php
@@ -12,21 +12,25 @@ $new_text_area = str_replace($bad_word, '***', $text_area);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Badword Result</title>
 </head>
 
-<body>
-    <div>
-        <?php echo $text_area; ?>
-        <?php echo strlen($text_area) ?>
+<body class="d-flex justify-content-center align-items-center vh-100 bg-warning p-4">
+    <div class="text-center gap-5  d-flex bg-dark p-5 rounded-3 shadow-lg text-white fw-bold">
+        <div>
+            <h3 class="text-warning mb-4">Original Text</h3>
+            <p> <?php echo $text_area; ?></p>
+            <div> <?php echo strlen($text_area) ?></div>
+        </div>
+        <div>
+            <h3 class="text-warning mb-4">Edited Text</h3>
+            <p> <?php echo $new_text_area ?></p>
+            <div> <?php echo strlen($new_text_area) ?>
+            </div>
+        </div>
     </div>
-
-    <div>
-        <?php echo $new_text_area ?>
-        <?php echo strlen($new_text_area) ?>
-
-    </div>
-
 </body>
 
 </html>
